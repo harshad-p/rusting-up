616eb1de60473849ded71abda7696f9a7deffbe6
Harshad Paradkar <harshad800@gmail.com>
2023-08-02 21:59:14 +0530

    Test shadowing of variables.
    Instead of declaring a variable (x) as mutable, a variable can be made mutable temporarily by creating a new shadow variable. It retains the last value assigned during initialization.

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,14 @@
 fn main() {
+    println!("Showcasing Mutability");
     let mut x = -1.1;
     println!("The value of x is: {x}");
     x = 1.2;
-    println!("The value of x is: {x}");
+    println!("The value of x after mutating it is: {x}");
+
+    println!("Showcasing Shadowing");
+    let y = 2;
+    println!("The value of y is: {y}");
+    let y = y * 2;
+    println!("The value of y after doubling it is: {y}");
+
 }
