d7d17f4ee7ccd5430d983f9815d5c75a3c947ba9
Harshad Paradkar <harshad800@gmail.com>
2023-08-04 23:43:12 +0530

    Tried various ways to assign value to an unsigned int.

--- data_types_scalar_integer/src/main.rs
+++ data_types_scalar_integer/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let unsigned_8_int :u8 = 255;
+    // This does not work
+    // Cannot assign a signed int to unsigned int. 
+    // Cannot even negate an int and assign it. 
+    // let unsigned_8_int_Neg :u8 = -1 * unsigned_8_int; 
+    println!("This is an unsigned 8-bit integer: {unsigned_8_int}");
 }
