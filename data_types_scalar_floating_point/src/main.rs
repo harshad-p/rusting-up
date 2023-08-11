ba49ca57c81d072eaef0318d44640db3f643ac33
Harshad Paradkar <harshad800@gmail.com>
2023-08-11 23:07:34 +0530

    Init 32 and 64 bit fp numbers.

--- data_types_scalar_floating_point/src/main.rs
+++ data_types_scalar_floating_point/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    // It is necessary to add a decimal point, or else it won't compile. 
+    // Although, the decimal point is not displayed on the console. 
+    let floating_point_32 :f32 = -255.0;
+
+    // If the type is not specified, it takes the type of the variable being assigned. 
+    // It is not initialized as a 64-bit floating point number. 
+    // Also, a 32-bit fp number cannot be assigned to 64-bit variable. 
+    // let floating_point_64 :f64 = floating_point_32; // This does not work
+    
+    let floating_point_64 :f64 = -255255.0;
+
+    println!("This is a signed 32-bit floating-point number: {floating_point_32}");
+    println!("This is a signed 64-bit floating-point number: {floating_point_64}");
 }
