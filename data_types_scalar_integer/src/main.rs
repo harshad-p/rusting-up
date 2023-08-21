39701909a3778295f743912495c78aeb4b18d197
Harshad Paradkar <harshad800@gmail.com>
2023-08-21 20:54:03 +0530

    Trying to see if low to high or high to low bit ints can be assigned to each other.

--- data_types_scalar_integer/src/main.rs
+++ data_types_scalar_integer/src/main.rs
@@ -1,8 +1,19 @@
 fn main() {
     let unsigned_8_int :u8 = 255;
+    println!("This is an unsigned 8-bit integer: {unsigned_8_int}");
+
     // This does not work
     // Cannot assign a signed int to unsigned int. 
     // Cannot even negate an int and assign it. 
     // let unsigned_8_int_Neg :u8 = -1 * unsigned_8_int; 
-    println!("This is an unsigned 8-bit integer: {unsigned_8_int}");
+
+    // Assign a lower bit integer to a higher bit integer. 
+    //let u_32_int: u32 = 32;
+    //let u_64_int: u64 = u_32_int;
+    //println!("This is an unsigned 64-bit integer assigned from 32-bit integer: {u_64_int}");
+    
+    // Assign a higher bit integer to a lower bit integer. 
+    //let u_16_int: u16 = 16;
+    //let u_8_int: u8 = u_16_int;    
+    //println!("This is an unsigned 8-bit integer assigned from 16-bit integer: {u_8_int}");
 }
