e9e9ead629c84c12c619140bbb2ec46d50b3d485
Harshad Paradkar <harshad800@gmail.com>
2023-08-23 20:41:11 +0530

    Create new project for testing character type variables.

---
+++ data_types_scalar_character/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let c = 'z';
+    println!("Regular character: {c}");
+    let z: char = 'ℤ'; // with explicit type annotation
+    println!("Explicit character initialization: {z}");
+    let heart_eyed_cat = '😻';
+    println!("Emoji character: {heart_eyed_cat}");
+}
