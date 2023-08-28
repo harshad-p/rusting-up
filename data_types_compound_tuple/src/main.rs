dc4be40ea1b7ffddb4f49e2d0be798fee32dbba1
Harshad Paradkar <harshad800@gmail.com>
2023-08-28 22:15:26 +0530

    Update deconstructed Tuple names.

--- data_types_compound_tuple/src/main.rs
+++ data_types_compound_tuple/src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
     // Default Tuple initialization
-    let t1 = (1, 2.2, 'z', true);
+    let t1 = (-1, 2.2, 'z', true);
     // All tuples need to be deconstruction when using this pattern matching. 
     // If only the first 2 are needed, still all 4 need to be deconstructed. 
-    let (unsigned_8_int, floating_point_32, character, boolean) = t1;
-    println!("Tuples: {unsigned_8_int}, {floating_point_32}, {character}, {boolean}");
+    let (integer, floating_point, character, boolean) = t1;
+    println!("Tuples: {integer}, {floating_point}, {character}, {boolean}");
     
     // Explicit type specification
     let t2: (u8, f32, char, bool) = (11, 22.22, 'a', true);
