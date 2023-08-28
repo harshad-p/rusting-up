8bab17f08795f3957020e95240d3e6b94e775cab
Harshad Paradkar <harshad800@gmail.com>
2023-08-28 22:18:11 +0530

    Use index based Tuple access.

--- data_types_compound_tuple/src/main.rs
+++ data_types_compound_tuple/src/main.rs
@@ -7,9 +7,12 @@ fn main() {
     println!("Tuples: {integer}, {floating_point}, {character}, {boolean}");
     
     // Explicit type specification
-    let t2: (u8, f32, char, bool) = (11, 22.22, 'a', true);
-    let (unsigned_8_int, floating_point_32, character, boolean) = t2;
-    println!("Tuples: {unsigned_8_int}, {floating_point_32}, {character}, {boolean}");
+    let t2: (u8, f32, char, bool) = (11, 22.22, 'a', false);
+    // Use index based access if not all tuples are required. 
+    let unsigned_8_int = t2.0;
+    let floating_point_32 = t2.1;
+    let boolean = t2.3;
+    println!("Tuples: {unsigned_8_int}, {floating_point_32}, {boolean}");
 
     let _t3 = (); // Cannot access value in empty tuple. 
 }
