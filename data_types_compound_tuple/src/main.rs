3d97129672cbb8a7e543454766b6fab2698ca05a
Harshad Paradkar <harshad800@gmail.com>
2023-08-28 21:55:36 +0530

    Create new project for testing Tuple type variables.

---
+++ data_types_compound_tuple/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // Default Tuple initialization
+    let t1 = (1, 2.2, 'z', true);
+    // All tuples need to be deconstruction when using this pattern matching. 
+    // If only the first 2 are needed, still all 4 need to be deconstructed.  
+    let (unsigned_8_int, floating_point_32, character, boolean) = t1;
+    println!("Tuples: {unsigned_8_int}, {floating_point_32}, {character}, {boolean}");
+}
