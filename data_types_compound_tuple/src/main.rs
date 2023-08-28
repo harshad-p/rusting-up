a53d9b7785f0b5c33ccb924da271660bab39675d
Harshad Paradkar <harshad800@gmail.com>
2023-08-28 22:07:53 +0530

    I take back the previous comment.

--- data_types_compound_tuple/src/main.rs
+++ data_types_compound_tuple/src/main.rs
@@ -3,11 +3,6 @@ fn main() {
     let t1 = (1, 2.2, 'z', true);
     // All tuples need to be deconstruction when using this pattern matching. 
     // If only the first 2 are needed, still all 4 need to be deconstructed. 
-    // Also note how an int is initialized as a signed 32-bit integer in Rust, 
-    // and Rust does not allow you to change the bitness after initialization. 
-    // But here Rust does allow you to specify a non-default bitness. 
-    // (I would have assumed that Rust would have initialized this integer as i32.)
-    // Same for the floating-point number. 
     let (unsigned_8_int, floating_point_32, character, boolean) = t1;
     println!("Tuples: {unsigned_8_int}, {floating_point_32}, {character}, {boolean}");
 }
