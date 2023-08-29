902078472d884e086343eabee1baa8e576d929d0
Harshad Paradkar <harshad800@gmail.com>
2023-08-29 22:27:03 +0530

    Create new project for testing Array type variables.

---
+++ data_types_compound_array/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // Default Array initialization
+    let a1 = [-3, -2, -1, 0, 1, 2, 3];
+    // I do not know yet how to access the array elements directly in the print stmt. 
+    let first = a1[0];
+    let third = a1[2];
+    let seventh = a1[6];
+    println!("Array elements: First: {first}, Third: {third}, Seventh: {seventh}");
+}
