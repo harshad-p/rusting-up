4c47f1bff4da6a8061967073f93d595656207527
Harshad Paradkar <harshad800@gmail.com>
2023-08-29 22:37:12 +0530

    Try array initialization by specifying the type and length

--- data_types_compound_array/src/main.rs
+++ data_types_compound_array/src/main.rs
@@ -6,4 +6,12 @@ fn main() {
     let third = a1[2];
     let seventh = a1[6];
     println!("Array elements: First: {first}, Third: {third}, Seventh: {seventh}");
+
+    // Array initialization by specifying the type and length
+    let a2: [i8; 7] = [-3, -2, -1, 0, 1, 2, 3];
+    // I do not know yet how to access the array elements directly in the print stmt. 
+    let first = a2[0];
+    let third = a2[2];
+    let seventh = a2[6];
+    println!("Array elements: First: {first}, Third: {third}, Seventh: {seventh}");
 }
